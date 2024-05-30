b9f64c09154cbf2931d57a7826df53d334fbe7b1
Eunika Farkasova <xfarkasovae@stuba.sk>
2024-05-30 14:09:34 +0200

    1. Open tlacitko

--- app.py
+++ app.py
@@ -14,3 +14,16 @@ thread_lock = Lock()
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
+
+@socketio.on('my_event', namespace='/test')
+def test_message(message):   
+    session['receive_count'] = session.get('receive_count', 0) + 1 
+    print(message)
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(target=background_thread, args=session._get_current_object())
+    emit('my_response', {'data': 'Connected', 'count': 0})
---
+++ static/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    namespace = '/test';
+    var socket;
+
+    $('#open').click(function(event) {
+        console.log("open");
+        socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
+        socket.on('connect', function() {
+          socket.emit('my_event', {data: 'I\'m connected!', value: 1}); 
+        });
+
+        socket.on('my_response', function(msg) {
+            $('#log').prepend('Received #'+msg.count+': '+JSON.stringify(msg.data)+'<br>').html();
+        });
+    }); 
+             
+});
