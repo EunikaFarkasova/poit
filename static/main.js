2d10366702137b589db2f6d992b4097a63f2bac7
Eunika Farkasova <xfarkasovae@stuba.sk>
2024-05-31 21:02:56 +0200

    5. graf

--- static/main.js
+++ static/main.js
@@ -1,4 +1,29 @@
 $(document).ready(function() {
+    var x = new Array();
+    var tempY = new Array();
+    var lightY = new Array();
+    var tempTrace;
+    var lightTrace;
+    var tempLayout = {
+        title: 'Temperature data',
+        xaxis: {
+            title: 'Datetime',
+            type: 'date',
+        },
+        yaxis: {
+            title: 'Value',
+        }
+    };
+    var lightLayout = {
+        title: 'Light data',
+        xaxis: {
+            title: 'Datetime',
+            type: 'date',
+        },
+        yaxis: {
+            title: 'Value',
+        }
+    };
     var temp;
     var light;
 
@@ -14,6 +39,31 @@ $(document).ready(function() {
 
         socket.on('my_response', function(msg) {
             $('#log').prepend('Received #'+msg.count+': '+JSON.stringify(msg.data)+'<br>').html();
+            x.push(msg.data.datetime);
+            console.log(msg.data.datetime)
+            if (temp) {
+                tempY.push(parseFloat(msg.data.temperature));
+                tempTrace = {
+                    x: x,
+                    y: tempY,
+                    name: 'value',
+                };
+                var tempTraces = new Array();
+                tempTraces.push(tempTrace);
+                Plotly.newPlot($('#temp-plot')[0], tempTraces, tempLayout); 
+            }
+
+            if (light) {
+                lightY.push(parseFloat(msg.data.light));
+                lightTrace = {
+                    x: x,
+                    y: lightY,
+                    name: 'value',
+                };      
+                var lightTraces = new Array();
+                lightTraces.push(lightTrace);
+                Plotly.newPlot($('#light-plot')[0], lightTraces, lightLayout);  
+            }
         });
     });
 
@@ -43,6 +93,11 @@ $(document).ready(function() {
     $('.buttonVal').click(function(event) {
         const val = event.target.value
         console.log(val)
+        if (val == 'true') {
+            x = new Array();
+            tempY = new Array();
+            lightY = new Array();
+        }
         socket.emit('click_event', {value: val});
         return false; 
       });
