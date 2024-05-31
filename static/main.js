00a22fbd993552119fad2d1c68245ae6e10217c0
Eunika Farkasova <xfarkasovae@stuba.sk>
2024-05-31 21:27:35 +0200

    6. cifernik

--- static/main.js
+++ static/main.js
@@ -1,4 +1,120 @@
 $(document).ready(function() {
+    var tempGauge = new RadialGauge({
+        renderTo: 'tempGauge',
+        width: 300,
+        height: 300,
+        units: "°C",
+        minValue: -50,
+        maxValue: 50,
+        majorTicks: [
+            "-50",
+            "-45",
+            "-40",
+            "-35",
+            "-30",
+            "-25",
+            "-20",
+            "-15",
+            "-10",
+            "-5",
+            "0",
+            "5",
+            "10",
+            "15",
+            "20",
+            "25",
+            "30",
+            "35",
+            "40",
+            "45",
+            "50"
+        ],
+        minorTicks: 2,
+        strokeTicks: true,
+        highlights: [
+            {
+                "from": -50,
+                "to": -35,
+                "color": "rgba(200, 50, 50, .75)"
+            },
+            {
+                "from": 35,
+                "to": 50,
+                "color": "rgba(50, 200, 50, .75)"
+            }
+        ],
+        colorPlate: "#fff",
+        borderShadowWidth: 0,
+        borders: false,
+        needleType: "arrow",
+        needleWidth: 2,
+        needleCircleSize: 7,
+        needleCircleOuter: true,
+        needleCircleInner: false,
+        animationDuration: 1500,
+        animationRule: "linear"
+    });
+    tempGauge.draw();
+    tempGauge.value = "0";
+
+    var lightGauge = new RadialGauge({
+        renderTo: 'lightGauge',
+        width: 300,
+        height: 300,
+        units: "adcVal",
+        minValue: 0,
+        maxValue: 4095,
+        majorTicks: [
+            "0",
+            "200",
+            "400",
+            "600",
+            "800",
+            "1000",
+            "1200",
+            "1400",
+            "1600",
+            "1800",
+            "2000",
+            "2200",
+            "2400",
+            "2600",
+            "2800",
+            "3000",
+            "3200",
+            "3400",
+            "3600",
+            "3800",
+            "4095"
+        ],
+        minorTicks: 2,
+        strokeTicks: true,
+        highlights: [
+            {
+                "from": 0,
+                "to": 400,
+                "color": "rgba(200, 50, 50, .75)"
+            },
+            {
+                "from": 3400,
+                "to": 4095,
+                "color": "rgba(50, 200, 50, .75)"
+            }
+        ],
+        colorPlate: "#fff",
+        borderShadowWidth: 0,
+        borders: false,
+        needleType: "arrow",
+        needleWidth: 2,
+        needleCircleSize: 7,
+        needleCircleOuter: true,
+        needleCircleInner: false,
+        animationDuration: 1500,
+        animationRule: "linear"
+    });
+    lightGauge.draw();
+    lightGauge.value = "0";
+
     var x = new Array();
     var tempY = new Array();
     var lightY = new Array();
@@ -43,6 +159,7 @@ $(document).ready(function() {
             console.log(msg.data.datetime)
             if (temp) {
                 tempY.push(parseFloat(msg.data.temperature));
+                tempGauge.value = parseFloat(msg.data.temperature);
                 tempTrace = {
                     x: x,
                     y: tempY,
@@ -55,6 +172,7 @@ $(document).ready(function() {
 
             if (light) {
                 lightY.push(parseFloat(msg.data.light));
+                lightGauge.value = parseFloat(msg.data.light);
                 lightTrace = {
                     x: x,
                     y: lightY,
