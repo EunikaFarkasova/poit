df095b682c573601b220ca9974e448c14faaa8be
Eunika Farkasova <xfarkasovae@stuba.sk>
2024-06-02 16:43:22 +0200

    7. databaza, 8. subor

--- app.py
+++ app.py
@@ -2,12 +2,25 @@ from threading import Lock
 from flask import Flask, render_template, session, request, jsonify, url_for, send_from_directory
 from flask_socketio import SocketIO, emit, disconnect    
 import time
+import json
+import MySQLdb
+import configparser as ConfigParser
 import serial
+import random
 from datetime import datetime
+import os
 
 async_mode = None
 
 app = Flask(__name__)
+
+config = ConfigParser.ConfigParser()
+config.read('config.cfg')
+myhost = config.get('mysqlDB', 'host')
+myuser = config.get('mysqlDB', 'user')
+mypasswd = config.get('mysqlDB', 'passwd')
+mydb = config.get('mysqlDB', 'db')
+print(myhost)
 
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
@@ -22,7 +35,8 @@ def background_thread(args):
     dataList = []
     temperature = 'false'
     light = 'false'         
-    enable = 'false'           
+    enable = 'false'
+    db = MySQLdb.connect(host=myhost,user=myuser,passwd=mypasswd,db=mydb)            
     while True:
         if args:
           temperature = dict(args).get('temperature')
@@ -37,6 +51,7 @@ def background_thread(args):
           line = ser.readline().decode('utf-8').rstrip()
           data = json.loads(line)
           print(data)
+          #num = random.random()
           dataDict = {
             "datetime": datetimeString
           }
@@ -52,6 +67,19 @@ def background_thread(args):
                         {'data': dataDict, 'count': count},
                         namespace='/test')
         else:
+          if len(dataList)>0:
+            print(str(dataList))
+            fuj = str(dataList).replace("'", "\"")
+            print(fuj)
+            cursor = db.cursor()
+            cursor.execute("SELECT MAX(id) FROM graph")
+            maxid = cursor.fetchone()[0]
+            if maxid is None:
+              new_id = 1
+            else:
+              new_id = maxid + 1
+            cursor.execute("INSERT INTO graph (id, hodnoty) VALUES (%s, %s)", (new_id, fuj))
+            db.commit()
           dataList = []
           count = 0
 
@@ -59,6 +87,47 @@ def background_thread(args):
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
+@app.route('/archive')
+def archive():
+    return render_template('archive.html', async_mode=socketio.async_mode)
+    
+@app.route('/graph')
+def graph():
+    return render_template('graph.html', async_mode=socketio.async_mode)
+
+@app.route('/db')
+def db():
+  db = MySQLdb.connect(host=myhost,user=myuser,passwd=mypasswd,db=mydb)
+  cursor = db.cursor()
+  cursor.execute('''SELECT id FROM  graph''')
+  rv = cursor.fetchall()
+  response = [{"id": row[0]} for row in rv]
+  return jsonify(response)
+
+@app.route('/dbdata/<string:num>', methods=['GET', 'POST'])
+def dbdata(num):
+  db = MySQLdb.connect(host=myhost,user=myuser,passwd=mypasswd,db=mydb)
+  cursor = db.cursor()
+  print(num)
+  cursor.execute("SELECT hodnoty FROM  graph WHERE id=%s", num)
+  rv = cursor.fetchone()
+  return str(rv[0])
+  
+@app.route('/downloadfile/<string:num>', methods=['GET', 'POST'])
+def downloadfile(num):
+  filepath = "static/files/data.txt"
+  directory = os.path.dirname(filepath)
+  filename = os.path.dirname(filepath)
+  downloadpath = os.path.join(app.root_path, 'static/files')
+  print(downloadpath)
+  
+  result = dbdata(num)
+  
+  with open(filepath, "w") as fo:
+    fo.write("%s\r\n" %result)
+    
+  return send_from_directory(directory='static', filename='files/data.txt', as_attachment=True)
+  
 @socketio.on('my_event', namespace='/test')
 def test_message(message):   
     session['receive_count'] = session.get('receive_count', 0) + 1 
@@ -72,12 +141,13 @@ def test_message(message):
       data["light"] = message['light']
     emit('my_response',
          {'data': data, 'count': session['receive_count']})
-
+ 
 @socketio.on('disconnect_request', namespace='/test')
 def disconnect_request():
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
          {'data': 'Disconnected!', 'count': session['receive_count']})
+    disconnect()
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
@@ -86,6 +156,10 @@ def test_connect():
         if thread is None:
             thread = socketio.start_background_task(target=background_thread, args=session._get_current_object())
     emit('my_response', {'data': 'Connected', 'count': 0})
+
+@socketio.on('click_event', namespace='/test')
+def db_message(message):   
+    session['enable'] = message['value']    
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
--- static/main.js
+++ static/main.js
@@ -140,6 +140,7 @@ $(document).ready(function() {
             title: 'Value',
         }
     };
+
     var temp;
     var light;
 
@@ -147,81 +148,82 @@ $(document).ready(function() {
     var socket;
 
     $('#open').click(function(event) {
-        console.log("open");
+      console.log("open");
         socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
         socket.on('connect', function() {
           socket.emit('my_event', {data: 'I\'m connected!', value: 1}); 
-        });
+      });
 
-        socket.on('my_response', function(msg) {
-            $('#log').prepend('Received #'+msg.count+': '+JSON.stringify(msg.data)+'<br>').html();
-            x.push(msg.data.datetime);
-            console.log(msg.data.datetime)
-            if (temp) {
-                tempY.push(parseFloat(msg.data.temperature));
-                tempGauge.value = parseFloat(msg.data.temperature);
-                tempTrace = {
-                    x: x,
-                    y: tempY,
-                    name: 'value',
-                };
-                var tempTraces = new Array();
-                tempTraces.push(tempTrace);
-                Plotly.newPlot($('#temp-plot')[0], tempTraces, tempLayout); 
-            }
+      socket.on('my_response', function(msg) {
+          $('#log').prepend('Received #'+msg.count+': '+JSON.stringify(msg.data)+'<br>').html();
 
-            if (light) {
-                lightY.push(parseFloat(msg.data.light));
-                lightGauge.value = parseFloat(msg.data.light);
-                lightTrace = {
-                    x: x,
-                    y: lightY,
-                    name: 'value',
-                };      
-                var lightTraces = new Array();
-                lightTraces.push(lightTrace);
-                Plotly.newPlot($('#light-plot')[0], lightTraces, lightLayout);  
-            }
-        });
-    });
+          x.push(msg.data.datetime);
+          console.log(msg.data.datetime)
+          if (temp) {
+              tempY.push(parseFloat(msg.data.temperature));
+              tempGauge.value = parseFloat(msg.data.temperature);
+              tempTrace = {
+                  x: x,
+                  y: tempY,
+                  name: 'value',
+              };
+              var tempTraces = new Array();
+              tempTraces.push(tempTrace);
+              Plotly.newPlot($('#temp-plot')[0], tempTraces, tempLayout); 
+          }
+
+          if (light) {
+              lightY.push(parseFloat(msg.data.light));
+              lightGauge.value = parseFloat(msg.data.light);
+              lightTrace = {
+                  x: x,
+                  y: lightY,
+                  name: 'value',
+              };      
+              var lightTraces = new Array();
+              lightTraces.push(lightTrace);
+              Plotly.newPlot($('#light-plot')[0], lightTraces, lightLayout);  
+          }
+      });
+    }); 
+    
 
     $('#setCheckboxes').click(function(event) {
-        temp = $('#temperature_check')[0].checked
-        light = $('#light_check')[0].checked
-  
-        if (temp) {
-            $('#temp-wrapper').show()
-        } else {
-            $('#temp-wrapper').hide()
-        }
-        if (light) {
-            $('#light-wrapper').show()
-        } else {
-            $('#light-wrapper').hide()
-        }
-  
-          socket.emit('my_event', {
-            temperature: temp,
-            light: light
-          });
-          
-          return false; 
-    });
-    
+      temp = $('#temperature_check')[0].checked
+      light = $('#light_check')[0].checked
+
+      if (temp) {
+          $('#temp-wrapper').show()
+      } else {
+          $('#temp-wrapper').hide()
+      }
+      if (light) {
+          $('#light-wrapper').show()
+      } else {
+          $('#light-wrapper').hide()
+      }
+
+        socket.emit('my_event', {
+          temperature: temp,
+          light: light
+        });
+        
+        return false; 
+      });
     $('.buttonVal').click(function(event) {
         const val = event.target.value
         console.log(val)
         if (val == 'true') {
-            x = new Array();
-            tempY = new Array();
-            lightY = new Array();
+          x = new Array();
+          tempY = new Array();
+          lightY = new Array();
         }
         socket.emit('click_event', {value: val});
         return false; 
       });
-
     $('form#disconnect').submit(function(event) {
         socket.emit('disconnect_request');
         return false; 
-    });   
-});
+      });   
+             
+    });
